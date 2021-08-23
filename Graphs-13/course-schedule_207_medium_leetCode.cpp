d37d1de2027776bc05398e6af5f265dbd0f35736
Prashant <piseprashant@johndeere.com>
2021-08-23 20:01:40 +0530

    concept explained

--- Graphs-13/course-schedule_207_medium_leetCode.cpp
+++ Graphs-13/course-schedule_207_medium_leetCode.cpp
@@ -25,6 +25,10 @@ public:
         {
             if (! loop.count(p.first))
             {
+		// 
+		// step 2 - 1, empty loop, adj List - m 
+		// step 13 - now take key 2
+		// step 16 - now take key 3 that has 2 values 1,2
                 if (checkLoop(p.first, loop, m))
                 {
                     return false;
@@ -37,6 +41,10 @@ public:
     
     bool checkLoop(int begin, unordered_map<int, int>& loop, unordered_map<int, list<int>>& m)
     {
+	// step 3 - loop is empty skip
+	// step 8 - has key 0 has values, as 0 has no values no further dfs skip
+	// step 14 - for key 2, no values in already explored loops. 
+	// step 17 - check if we detect any loops for key 3. so its not traced yet
         if (loop.count(begin))
         {
             if (loop[begin])
@@ -45,23 +53,36 @@ public:
             }
             else
             {
+		// step  16 - as 0 was already explored and said no loops return no loops false
                 return false;
             }
         }
-
+	// step 4 - visited list - begin elements is say we could have a loop. set 1
+	// step 9 - say 0 is visited
+	// step 15 - so lets say we have loop for key 2
+	// step 18 - iterate for key 3 and lets say it has cycle
         loop[begin] = 1;
+	// step 5 - check if given key has elements. Ans is yes . Key is 1 value - 0
+	// step  10 - no elements for 0 to enter dfs check loop
         if (m.count(begin))
         {
+	    //step 6 -  so iterate to all the elements of that key and do DFS. where p = 0
             for (auto& p : m[begin])
             {
+		//step 7 -  no check value 0 as key and do recurrsive DFS
+		// step 12, as 0 no loop take next from adj list i.e key -2 
                 if (checkLoop(p, loop, m))
                 {
                     return true;
                 }
             }
         }
+	// step 11 - confirmed no loop for value 0
         loop[begin] = 0;    //confirmed no loop
         
         return false;
     }
 };
+
+// same concept using set and hash map in python.
+// https://www.youtube.com/watch?v=EgI5nU9etnU
